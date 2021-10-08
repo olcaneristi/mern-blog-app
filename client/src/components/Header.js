d0b47c0773ccb4a0a59d6aa3b61e29dd4f5f0b32
oğulcan erişti <ogulcaneristi123@gmail.com>
2021-10-08 13:48:52 +0300

    add navlink

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { chakra, useColorModeValue, Flex, Button } from '@chakra-ui/react';
+import { chakra, useColorModeValue, Flex, Button, Link } from '@chakra-ui/react';
 import Logo from '../images/Logo';
 import AddPostForm from './AddPostForm';
 import { useDisclosure } from '@chakra-ui/hooks';
@@ -13,13 +13,31 @@ const Header = () => {
       <chakra.header bg={bg} w="full" px={5} py={1} shadow="md" mx="auto">
         <Flex align="center" justify="space-between" mx="auto" maxW="1420px">
           <Flex>
-            <chakra.a href="/posts" title="Choc Home Page" display="flex" alignItems="center">
+            <chakra.a href={'/'} title="Choc Home Page" display="flex" alignItems="center">
               <Logo />
             </chakra.a>
           </Flex>
-          <Button colorScheme="teal" size="sm" onClick={onOpen}>
-            New Post
-          </Button>
+          <Flex align="center">
+            <Link
+              display="block"
+              color={useColorModeValue('gray.800', 'white')}
+              fontWeight="bold"
+              fontSize="lg"
+              href={'/posts'}
+              px={2}
+              py={1}
+              rounded={'md'}
+              _hover={{
+                textDecoration: 'none',
+                bg: useColorModeValue('gray.200', 'gray.700'),
+              }}
+            >
+              Posts
+            </Link>
+            <Button ml="10" colorScheme="teal" size="sm" onClick={onOpen}>
+              New Post
+            </Button>
+          </Flex>
         </Flex>
       </chakra.header>
 
